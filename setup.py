0e33120c5654f11fb544f420bbf09e0b5bda927d
Stéphane Blondon <stephane.blondon@gmail.com>
2015-01-01 22:17:04 +0100

    update url, add classifier informations in setup

--- setup.py
+++ setup.py
@@ -6,10 +6,18 @@ setup(
         name="pluralizefr",
         description="pluralize for french language",
         version="0.0.1",
-        url="https://github.com/sblondon/pluralizefr",
+        url="https://github.com/yaal-fr/pluralizefr",
+        license = "BSD",
         packages=[
             "pluralizefr",
             ],
         test_suite='tests',
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "License :: OSI Approved :: BSD License",
+            ],
         )
 
